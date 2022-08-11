0b0d9319d6cb2ae4546eba42ebdefac5e3841ba8
sunny <sunny@iandothers.org>
2022-08-11 17:33:24 +0530

    Implemented counter and changing display as part of https://www.youtube.com/watch?v=7m_bwXWIbwc&list=PLr3kuDAFECjZhW-p56BoVB7SubdUHBVQT&index=2

--- extension.js
+++ extension.js
@@ -1,7 +1,16 @@
 const St = imports.gi.St; //For UI Elements such as button
 const Main = imports.ui.main; //For UI Elements such as panel
+const MainLoop = imports.mainloop;
 
 let panelButton, panelButtonText;
+let timeout; //Source reference for installing a timeout.
+let counter = 0; // An integer for counting stuff
+
+function setButtonText() {
+    counter++; //Count
+    panelButtonText.set_text( counter.toString() );//Change text of label
+    return true; //If we return void or false loop will stop
+}
 
 function init() {
     //A new button
@@ -20,9 +29,11 @@ function init() {
 function enable() {
     //Add button to in Main panel's left box
     Main.panel._leftBox.insert_child_at_index(panelButton, 1);
+    timeout = MainLoop.timeout_add_seconds(1.0, setButtonText); //Launch timeout source code every 1 second which points to setButtonText
 }
 
 function disable() {
+    MainLoop.source_remove(timeout); //Uninstall timeout source code
     //Remove button from Main panel's left box
     Main.panel._leftBox.remove_child(panelButton);
 }
