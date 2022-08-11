868828f313bf097c6b3fffb1aa165962aa22279f
sunny <sunny@iandothers.org>
2022-08-11 17:14:59 +0530

    Added comments

--- extension.js
+++ extension.js
@@ -4,21 +4,26 @@ const Main = imports.ui.main; //For UI Elements such as panel
 let panelButton, panelButtonText;
 
 function init() {
+    //A new button
     panelButton = new St.Bin({
         style_class : "panel-button"
     });
+    //A Label
     panelButtonText = new St.Label({
         style_class : "examplePanelText",
         text : "Starting..."
     });
+    //Add label to button
     panelButton.set_child(panelButtonText);
 }
 
 function enable() {
+    //Add button to in Main panel's left box
     Main.panel._leftBox.insert_child_at_index(panelButton, 1);
 }
 
 function disable() {
+    //Remove button from Main panel's left box
     Main.panel._leftBox.remove_child(panelButton);
 }
 
